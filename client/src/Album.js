e92a64e1f734a6d5bc05f87468dcdaf893dfb87f
balintataw <balintataw2@gmail.com>
2018-03-29 17:10:24 -0700

    added album images component

--- client/src/Album.js
+++ client/src/Album.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 
 export class Album extends Component {
     state = {
-        images: []
+        images: [],
+        albumName: ''
     }
 
     componentDidMount(props) {
         const id = this.props.match.params.id
         axios.get(`http://localhost:3001/albums/${id}?_embed=images`).then(resp => {
-            console.log(resp.data.images)
+            console.log(resp.data)
             this.setState({
-                images: resp.data.images
+                images: resp.data.images,
+                albumName: resp.data.albumName
             })
         })
     }
@@ -19,7 +21,17 @@ export class Album extends Component {
     render() {
         return (
             <div className="album-container">
-                album page
+                <header className="header">
+                    <h3>{this.state.albumName}</h3>
+                </header>
+                <div className="album-images">
+                    {this.state.images.map((image, i) => {
+                        return  <div className="images-container" key={"image" + i}>
+                                    <img src={image.imageURL}  alt=""/>
+                                    <h3 className="image-name">{image.imageName}</h3>
+                                </div>
+                    })}
+                </div>
             </div>
         )
     }
