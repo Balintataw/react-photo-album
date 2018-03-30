37fb40abb9a771bd5fcad0e3afa4d722e2a53a6d
balintataw <balintataw2@gmail.com>
2018-03-30 13:45:32 -0700

    phot page cycles next/prev but not strictly with album

--- client/src/Photo.js
+++ client/src/Photo.js
@@ -5,31 +5,45 @@ import store from './store'
 
 export class Photo extends Component {
     state = {
-        image: ''
+        image: '',
+        imageId: '',
+        albumId: ''
     }
   
     componentDidMount(props) {
         const id = this.props.match.params.id
-
+        
         getImageById(id)
         this.unsubscribe = store.subscribe(() => {
             const state = store.getState()
             this.setState({
-                image: state.currentImage
+                image: state.currentImage,
+                imageId: state.currentImage.id,
+                albumId: state.currentImage.albumId
             })
+            // console.log(this.state)
         })
     }
     componentWillUnmount() {
         this.unsubscribe()
+    }
+    next = () => {
+        getImageById(this.state.imageId + 1)
+        console.log(this.state.albumId)
+    }
+    previous = () => {
+        getImageById(this.state.imageId - 1)
+        console.log(this.state)
     }
 
     render() {
         return (
             <div className="foto-container">
                 <div className="photo-nav-bar">
-                    <Link to={'/album/' + this.state.image.albumId}>Previous</Link> 
+                    <Link onClick={this.previous} to={'/photo/' + this.state.imageId}>Previous</Link> 
                     <Link to={'/album/' + this.state.image.albumId}>Go back</Link> 
-                    <Link to={'/album/' + this.state.image.albumId}>Next</Link> 
+                    <Link onClick={this.next} to={'/photo/' + this.state.imageId}>Next</Link> 
+                    {/* <Link onClick={this.next()}>Next</Link>  */}
                 </div>
                 <div className="img-and-caption">
                     <img src={this.state.image.imageURL} id="single-foto-img" alt=""/>
