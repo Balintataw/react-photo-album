9f4048a3e80a3435b1bd642c7c689d3b604bf6c5
balintataw <balintataw2@gmail.com>
2018-03-30 23:10:37 -0700

    images need to cycle with albumId

--- client/src/Photo.js
+++ client/src/Photo.js
@@ -4,37 +4,25 @@ import {getImageById} from './photoAction'
 import {connect} from 'react-redux'
 
 export class Photo extends Component {
-    // state = {
-    //     image: '',
-    //     imageId: '',
-    //     albumId: ''
-    // }
   
     componentDidMount(props) {
         const id = this.props.match.params.id
-        
         getImageById(id)
-        // this.unsubscribe = store.subscribe(() => {
-        //     const state = store.getState()
-        //     this.setState({
-        //         image: state.currentImage,
-        //         imageId: state.currentImage.id,
-        //         albumId: state.currentImage.albumId
-        //     })
-        //     // console.log(this.state)
-        // })
     }
-    // componentWillUnmount() {
-    //     this.unsubscribe()
-    // }
-    // next = () => {
-    //     getImageById(this.props.imageId)
-    //     // console.log(this.state.albumId)
-    // }
-    // previous = () => {
-    //     getImageById(this.props.imageId)
-    //     // console.log(this.state)
-    // }
+
+    next = () => {
+        getImageById(this.props.imageId + 1)
+    }
+    previous = () => {
+        getImageById(this.props.imageId - 1)
+    }
+    componentWillReceiveProps(nextProps) {
+        console.log(this.props.albumId)
+        console.log(nextProps.albumId)
+        if(nextProps.albumId !== this.props.albumId) {
+            console.log('good')
+        }
+    }
 
     render() {
         return (
@@ -43,7 +31,8 @@ export class Photo extends Component {
                     <Link onClick={this.previous} to={'/photo/' + this.props.imageId}>Previous</Link> 
                     <Link to={'/album/' + this.props.albumId}>Go back</Link> 
                     <Link onClick={this.next} to={'/photo/' + this.props.imageId}>Next</Link> 
-                </div>{console.log(this.props)}
+                </div>
+                {/* {console.log(this.props)} */}
                 <div className="img-and-caption">
                     <img src={this.props.imageURL} id="single-foto-img" alt=""/>
                     <p id="image-caption">{this.props.imageName}</p>
@@ -57,7 +46,7 @@ function mapStateToProps(state) {
     console.log(state)
     return {
         image: state.currentImage.image,
-        imageId: state.currentImage.imageId,
+        imageId: state.currentImage.id,
         albumId: state.currentImage.albumId,
         imageURL: state.currentImage.imageURL,
         imageName: state.currentImage.imageName
